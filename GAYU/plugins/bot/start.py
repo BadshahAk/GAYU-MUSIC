e9fc624573954407e731473c54fcfba909f43e2e
⫷ Aɳαɳԃ Sԋαɾɱα ⫸ <91930171+BadshahAk@users.noreply.github.com>
2025-01-19 20:41:31 +0530

    Update start.py

--- GAYU/plugins/bot/start.py
+++ GAYU/plugins/bot/start.py
@@ -33,7 +33,7 @@ async def start_pm(client, message: Message, _):
     typing_message = await message.reply("<b>🦚𝐑𝐚𝐝𝐡𝐞🦚....🦚𝐑𝐚𝐝𝐡𝐞🦚</b>")  # Initial message
     
     # Simulate typing
-    typing_text = "<b>Anand Love Gayatri</b>"
+    typing_text = "Starting...🥀"
     
     for i in range(1, len(typing_text) + 1):  # Loop through each character
         try:
